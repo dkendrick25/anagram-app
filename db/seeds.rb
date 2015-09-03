d5896b9a63e1624a03d7e38d6058b99e734d2866
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2015-09-03 16:46:19 -0400

    Adding bigger dictionary file and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-require 'csv'    
- 
-csv_text = File.read('db/dictionary.csv')
+require 'csv'
+
+csv_text = File.read('db/words.csv')
 dictionary_words = CSV.parse(csv_text)
+
 dictionary_words.each do |row|
-    Word.create!(text: row[0])
+	Word.create!(text: row[0])
 end
